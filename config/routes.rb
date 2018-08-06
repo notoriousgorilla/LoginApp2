e217faa50946fe673a0f7995bef2a0b46c3c7e6b
Unknown <notoriousgorilla@gmail.com>
2018-08-06 11:13:25 +0200

    gemfile
    edited gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
     delete 'logout' => 'sessions#destroy'
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #this is a test comment for a commit
 end
